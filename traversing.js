fa9af81b485c5db1752195553500d29a71ee7a40
Kyle Creek <42050414+KyleCreek@users.noreply.github.com>
2023-11-08 15:17:06 -0800

    Update traversing.js
    Need to Finish Number 3

--- traversing.js
+++ traversing.js
@@ -1,11 +1,17 @@
 // Given the <body> element as variable body,
 // access the <main> node and log to the console.
 const body = document.querySelector('body');
+let main = body.children[0];
+console.log(main);
 
 // Given the <ul> element as variable ul,
 // access <body>  and log to the console.
 const ul = document.querySelector('ul');
+let body1 = ul.parentNode.parentNode;
+console.log(body1);
 
 // Given the <p> element as var p,
 // access the 3rd <li>  and log to the console.
+// To Do - Finish this assignment. 
 const p = document.querySelector('p');
+let main1 = p.parentNode;
