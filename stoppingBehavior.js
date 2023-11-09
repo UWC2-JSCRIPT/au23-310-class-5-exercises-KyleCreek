ceccb1845edebe2353b055f52c189ca1d2a19074
KyleCreek <kyle.a.creek@gmail.com>
2023-11-08 20:57:03 -0800

    I Think I finished the Last Buble Exercises, will need to review to ensure that I have completed everything, but this is basically done

--- stoppingBehavior.js
+++ stoppingBehavior.js
@@ -5,6 +5,16 @@ document.getElementById('cat').addEventListener('click', () => {
 
 // When clicked, "More info" link should alert "Here's some info"
 // instead of going to a new webpage
+const moreInfo = document.getElementById('more-info');
+moreInfo.addEventListener("click", (e) =>{
+  e.preventDefault();
+  alert("Here's some information");
+});
 
 // When the bark button is clicked, should alert "Bow wow!"
 // Should *not* alert "meow"
+const barkButton = document.getElementById('dog');
+barkButton.addEventListener("click", (e) =>{
+  e.stopPropagation();
+  alert('Bow Wow!');
+});
